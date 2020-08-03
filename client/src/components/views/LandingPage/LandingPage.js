623f751adbca3390096aba49a3fb5ea9c91a3e30
lqqokim <kis420404@gmail.com>
2020-08-04 04:13:57 +0900

    feat: proxy setting and add antd

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+
+const LandingPage = () => {
+
+    useEffect()
+
+    return (
+        <div>
+            LandingPage
+        </div>
+    );
+}
+
+export default LandingPage;
